346f6fe830bc4a0f1d2fdb24b5e92ac59d8c56d7
Gabriela Marin <78352574+LisaHouse@users.noreply.github.com>
2025-10-30 00:00:11 -0300

    first commit

---
+++ src/main/java/br/com/bakeflow/bakeflow/DataConfiguration.java
@@ -0,0 +1,12 @@
+package br.com.bakeflow.bakeflow;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Classe de configuração vazia pois todas as configurações do banco de dados
+ * foram movidas para o arquivo application.properties para melhor gerenciamento
+ * e para evitar configurações duplicadas.
+ */
+@Configuration
+public class DataConfiguration {
+}
