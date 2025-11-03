4c6738c3f001e5cc1c961339ea6393f511e03716
Gabriela Marin <78352574+LisaHouse@users.noreply.github.com>
2025-11-02 23:35:52 -0300

    Ajuste imports

--- src/main/java/br/com/bakeflow/bakeflow/model/Estoque.java
+++ src/main/java/br/com/bakeflow/bakeflow/model/Estoque.java
@@ -1,19 +1,15 @@
 package br.com.bakeflow.bakeflow.model;
-import java.io.Serializable;
-import java.util.List;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
+
 
 
 @Entity
--- src/main/java/br/com/bakeflow/bakeflow/model/Item_Pedido.java
+++ src/main/java/br/com/bakeflow/bakeflow/model/Item_Pedido.java
@@ -2,23 +2,16 @@ package br.com.bakeflow.bakeflow.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.sql.Date;
-import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "item_pedido")
