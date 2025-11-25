2f5813697a50cc1a07bad732e954fca1590fd8b3
Gabriela Marin <78352574+LisaHouse@users.noreply.github.com>
2025-11-24 23:41:50 -0300

    ajuste tag <nav>

--- src/main/java/br/com/bakeflow/bakeflow/controller/HomeController.java
+++ src/main/java/br/com/bakeflow/bakeflow/controller/HomeController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/index")
+@RequestMapping("/Index")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     public String home() {
-        return "index";
+        return "Index";
     }
 }
