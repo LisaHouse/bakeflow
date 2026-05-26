b553268b564a85bcfcc1053ea9760a29a1386644
Nathalia <nathalia.dluz@senacsp.edu.br>
2026-05-26 13:25:15 -0300

    Adiciona microserviço de fidelização e melhorias na interface

--- src/main/java/br/com/bakeflow/bakeflow/event/FidelizacaoListener.java
+++ src/main/java/br/com/bakeflow/bakeflow/event/FidelizacaoListener.java
@@ -1,20 +1,20 @@
 package br.com.bakeflow.bakeflow.event;
 
-import br.com.bakeflow.bakeflow.service.FidelizacaoService;
+import br.com.bakeflow.bakeflow.client.FidelizacaoClient;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FidelizacaoListener {
 
-    private final FidelizacaoService fidelizacaoService;
+    private final FidelizacaoClient fidelizacaoClient;
 
-    public FidelizacaoListener(FidelizacaoService fidelizacaoService) {
-        this.fidelizacaoService = fidelizacaoService;
+    public FidelizacaoListener(FidelizacaoClient fidelizacaoClient) {
+        this.fidelizacaoClient = fidelizacaoClient;
     }
 
     @EventListener
     public void aoFinalizarPedido(PedidoFinalizadoEvent event) {
-        fidelizacaoService.processarPedidoFinalizado(event);
+        fidelizacaoClient.enviarPedidoFinalizado(event);
     }
 }
